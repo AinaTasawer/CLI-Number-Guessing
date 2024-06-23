a3a32ee9f098890ec2ef0af8e803f9fff1d87c58
AinaTasawer <162625534+AinaTasawer@users.noreply.github.com>
2024-06-24 03:24:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+// Initialize a random number to be guessed by the user
+let randomNum = Math.floor(Math.random() * 6 + 1);
+// Prompt the user to guess the number
+const answer = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "Guess the number between 1-6 ",
+    },
+]);
+console.log(answer);
+// Check if the user's guessed number is correct
+if (answer.UserGuessedNumber === randomNum) {
+    console.log("Congratulations! you've guessed the correct number.");
+}
+else {
+    console.log("You've guessed the wrong number!");
+}
